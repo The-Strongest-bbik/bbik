06179c9462e99fc552550e6e85e96337d4504a84
Lee-Tae-Yun <cmskzkdl3698@naver.com>
2025-06-25 15:19:47 +0900

    init

---
+++ Bbik/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bbik
+//
+//  Created by 이태윤 on 6/25/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+
+
+}
+
