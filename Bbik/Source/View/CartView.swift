ece09c1e2ac17103c066868e2a083c6b73724631
가현 <102145605+rkgus24@users.noreply.github.com>
2025-06-27 20:19:39 +0900

    📌 Add: 장바구니 화면 TableView 파일 구조 정의
    Closes #31

---
+++ Bbik/Source/View/CartView.swift
@@ -0,0 +1,8 @@
+//
+//  CartView.swift
+//  Bbik
+//
+//  Created by 노가현 on 6/27/25.
+//
+
+// 전체 UI TabelView
---
+++ Bbik/Source/ViewController/CartViewController.swift
@@ -0,0 +1,8 @@
+//
+//  CartViewController.swift
+//  Bbik
+//
+//  Created by 노가현 on 6/27/25.
+//
+
+// View Data 연결 이벤트 처리
