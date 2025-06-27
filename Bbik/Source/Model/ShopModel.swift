336097dbe69bd1b475fbe37d7e9136afe7624741
Lee-Tae-Yun <cmskzkdl3698@naver.com>
2025-06-27 14:56:01 +0900

    📌 Add: 데이터 모델링
    Model 폴더 생성하여서 ShopModel, ShopDataService 생성하여 ViewController에 데이터확인 print문 추가
    
    Closes #23

--- Bbik/Source/Model/ShopModel.swift
+++ Bbik/Source/Model/ShopModel.swift
@@ -7,6 +7,15 @@
 //  Created by 이태윤 on 6/26/25.
 //
 
-struct ShopModel: Codable {
-    
+struct CategoryData: Codable, Hashable {
+    var category: String
+    var menus: [MenuData]
+}
+
+struct MenuData: Codable, Hashable {
+    var image: String
+    var name: String
+    var price: Int
+    var stock: Int
+    var sales: Int
 }
--- Bbik/Source/View/ShopView.swift
+++ Bbik/Source/View/ShopView.swift
@@ -10,13 +10,13 @@ import UIKit
 import SnapKit
 
 final class ShopView: UIView {
-	override init(frame: CGRect) {
-		super.init(frame: frame)
-
-		backgroundColor = .systemBackground
-	}
-
-	required init?(coder: NSCoder) {
-		fatalError("init(coder:) has not been implemented")
-	}
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        backgroundColor = .systemBackground
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
