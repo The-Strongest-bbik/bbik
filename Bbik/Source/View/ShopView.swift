28062db2712c5a636e3281063d833a6d3f37a556
Lee-Tae-Yun <cmskzkdl3698@naver.com>
2025-06-30 09:53:32 +0900

    🔨 Refactor: 코드블럭 수정

--- Bbik/Source/View/ShopView.swift
+++ Bbik/Source/View/ShopView.swift
@@ -26,17 +26,17 @@ final class ShopView: UIView {
         $0.backgroundColor = .separator
     }
 
-	lazy var shopCollectionView = UICollectionView(frame: .zero, collectionViewLayout: makeCollectionViewLayout()).then {
-		$0.backgroundColor = .systemBackground
-		$0.showsHorizontalScrollIndicator = false
-	}
+    lazy var shopCollectionView = UICollectionView(frame: .zero, collectionViewLayout: makeCollectionViewLayout()).then {
+        $0.backgroundColor = .systemBackground
+        $0.showsHorizontalScrollIndicator = false
+    }
 
-	let pageControl = UIPageControl().then {
-		$0.currentPage = 0
-		$0.pageIndicatorTintColor = UIColor(red: 179 / 255, green: 216 / 255, blue: 1.0, alpha: 1.0)
-		$0.currentPageIndicatorTintColor = .mainBlue
-		$0.isUserInteractionEnabled = true
-	}
+    let pageControl = UIPageControl().then {
+        $0.currentPage = 0
+        $0.pageIndicatorTintColor = UIColor(red: 179 / 255, green: 216 / 255, blue: 1.0, alpha: 1.0)
+        $0.currentPageIndicatorTintColor = .mainBlue
+        $0.isUserInteractionEnabled = true
+    }
 
     let bottomSeparatorView = UIView().then {
         $0.backgroundColor = .separator
@@ -83,19 +83,19 @@ final class ShopView: UIView {
         $0.textColor = .white
     }
 
-	override init(frame: CGRect) {
-		super.init(frame: frame)
+    override init(frame: CGRect) {
+        super.init(frame: frame)
 
-		backgroundColor = .systemBackground
+        backgroundColor = .systemBackground
         setCategoryViewUI()
-		setCollectionViewUI()
-		setPageControlUI()
+        setCollectionViewUI()
+        setPageControlUI()
         bottomCartStackViewUI()
-	}
+    }
 
-	required init?(coder: NSCoder) {
-		fatalError("init(coder:) has not been implemented")
-	}
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 
     private func setCategoryViewUI() {
         addSubview(categoryscrollView)
@@ -121,24 +121,24 @@ final class ShopView: UIView {
         }
     }
 
-	private func setCollectionViewUI() {
-		addSubview(shopCollectionView)
+    private func setCollectionViewUI() {
+        addSubview(shopCollectionView)
 
-		shopCollectionView.snp.makeConstraints { make in
+        shopCollectionView.snp.makeConstraints { make in
             make.top.equalTo(topSeparatorView.snp.bottom)
-			make.leading.trailing.equalToSuperview().inset(24)
-			make.height.equalTo(shopCollectionView.snp.width).multipliedBy(1.5)
-		}
-	}
+            make.leading.trailing.equalToSuperview().inset(24)
+            make.height.equalTo(shopCollectionView.snp.width).multipliedBy(1.5)
+        }
+    }
 
-	private func setPageControlUI() {
-		addSubview(pageControl)
+    private func setPageControlUI() {
+        addSubview(pageControl)
 
-		pageControl.snp.makeConstraints { make in
-			make.top.equalTo(shopCollectionView.snp.bottom).offset(16)
-			make.leading.trailing.equalToSuperview()
-		}
-	}
+        pageControl.snp.makeConstraints { make in
+            make.top.equalTo(shopCollectionView.snp.bottom).offset(16)
+            make.leading.trailing.equalToSuperview()
+        }
+    }
 
     private func bottomCartStackViewUI() {
         addSubview(bottomSeparatorView)
@@ -175,45 +175,45 @@ final class ShopView: UIView {
         }
     }
 
-	private func makeCollectionViewLayout() -> UICollectionViewLayout {
+    private func makeCollectionViewLayout() -> UICollectionViewLayout {
 
-		let itemSize = NSCollectionLayoutSize(
-			widthDimension: .fractionalWidth(0.5),
-			heightDimension: .fractionalHeight(1.0)
-		)
+        let itemSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(0.5),
+            heightDimension: .fractionalHeight(1.0)
+        )
 
-		let item = NSCollectionLayoutItem(layoutSize: itemSize)
-		item.contentInsets = NSDirectionalEdgeInsets(top: 9, leading: 9, bottom: 10, trailing: 10)
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        item.contentInsets = NSDirectionalEdgeInsets(top: 9, leading: 9, bottom: 10, trailing: 10)
 
-		let rowGroupSize = NSCollectionLayoutSize(
-			widthDimension: .fractionalWidth(1.0),
-			heightDimension: .fractionalHeight(1.0/3.0)
-		)
+        let rowGroupSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(1.0),
+            heightDimension: .fractionalHeight(1.0/3.0)
+        )
 
-		let rowGroup = NSCollectionLayoutGroup.horizontal(
-			layoutSize: rowGroupSize,
-			repeatingSubitem: item, count: 2
-		)
+        let rowGroup = NSCollectionLayoutGroup.horizontal(
+            layoutSize: rowGroupSize,
+            repeatingSubitem: item, count: 2
+        )
 
-		let pageGroupSize = NSCollectionLayoutSize(
-			widthDimension: .fractionalWidth(1.0),
-			heightDimension: .fractionalHeight(1.0)
-		)
+        let pageGroupSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(1.0),
+            heightDimension: .fractionalHeight(1.0)
+        )
 
-		let pageGroup = NSCollectionLayoutGroup.vertical(
-			layoutSize: pageGroupSize,
-			repeatingSubitem: rowGroup, count: 3
-		)
+        let pageGroup = NSCollectionLayoutGroup.vertical(
+            layoutSize: pageGroupSize,
+            repeatingSubitem: rowGroup, count: 3
+        )
 
-		let section = NSCollectionLayoutSection(group: pageGroup)
-		section.orthogonalScrollingBehavior = .groupPagingCentered
-		section.visibleItemsInvalidationHandler = { [weak self] _, offset, _ in
-			let pageIndex = round(offset.x / (self?.shopCollectionView.frame.width)!)
-			self?.pageControl.currentPage = Int(pageIndex)
-		}
+        let section = NSCollectionLayoutSection(group: pageGroup)
+        section.orthogonalScrollingBehavior = .groupPagingCentered
+        section.visibleItemsInvalidationHandler = { [weak self] _, offset, _ in
+            let pageIndex = round(offset.x / (self?.shopCollectionView.frame.width)!)
+            self?.pageControl.currentPage = Int(pageIndex)
+        }
 
-		return UICollectionViewCompositionalLayout(section: section)
-	}
+        return UICollectionViewCompositionalLayout(section: section)
+    }
 
     func setCategoryButtonsConfigure(_ categorys: [CategoryData]) {
         categoryStackView.addArrangedSubview(createCategoryButton(title: "전체", tag: -1))
